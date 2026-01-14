68cab434467aaf4ed8b89c2559441049d0dde93a
Andre Schauer <schauer.andre@outlook.de>
2026-01-14 13:44:23 +0100

    chore: ensure WASM files are tracked and not ignored

---
+++ src/wasm/sysml-kraken-wasm/sysml_kraken_wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const analyze_kraken_code: (a: number, b: number) => [number, number];
+export const init: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_start: () => void;
